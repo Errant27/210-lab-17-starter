a48cb7fdd096901588f13439794e0c90d4b3a981
Errant27 <kneil.sgo@gmail.com>
2024-10-03 18:23:11 -0700

    commit 2

--- main.cpp
+++ main.cpp
@@ -16,23 +16,25 @@ void deletes_list(Node *&);    // Function Prototype to delete list
 
 // (code would only function passing pointers by reference, might be an Xcode compiler thing)
 
-int main() {
+int main()
+{
    Node *head = nullptr;    // head pointer initialized to nullptr
     
     creates_list(head);    // Function call to create linked list
-    deletes_node(head);
-    inserts_node(head);
-    deletes_list(head);
+    deletes_node(head);    // Function call to delete node
+    inserts_node(head);    // Function call to insert node
+    deletes_list(head);    // Function call to delete linked list
     
    return 0;
 }
 
 
-void output(Node * &hd) {
-    Node *current = hd;    // current pointer points to hrad of list
+void output(Node * &hd)
+{
+    Node *current = hd;    // current pointer points to head of list
     int count = 1;    // count int variable to number each node in the list
     
-    if (!hd) {    // Output statment if list is empty
+    if (!hd) {    // Output statement if list is empty
        cout << "Empty list.\n";
        return;
    }
@@ -44,19 +46,18 @@ void output(Node * &hd) {
    cout << endl;
 }
 
-void creates_list(Node * &head) {
-    // create a linked list of size SIZE with random numbers 0-99
-       for (int i = 0; i < SIZE; i++) {
+void creates_list(Node * &head)    // function creates a linked list of size SIZE
+{
+       for (int i = 0; i < SIZE; i++) {    // list is filled with values of random numbers from 0-99
            int tmp_val = rand() % 100;
            Node *newVal = new Node;
    
-           // adds node at head
-           if (!head) { // if this is the first node, it's the new head
+           if (!head) {    // if head is not set yet, newVal is set as the new head
                head = newVal;
                newVal->next = nullptr;
                newVal->value = tmp_val;
            }
-           else { // its a second or subsequent node; place at the head
+           else {    // second or subsequent nodes placed at the head
                newVal->next = head;
                newVal->value = tmp_val;
                head = newVal;
@@ -65,18 +66,18 @@ void creates_list(Node * &head) {
        output(head);
 }
 
-void deletes_node(Node * &head) {
+void deletes_node(Node * &head)
+{
     Node *current = nullptr;
     Node *prev = nullptr;
     int entry;
     
-    cout << "Which node to delete? " << endl;
+    cout << "Which node to delete? " << endl;    // User prompted to enter the node they wish to delete
     output(head);
     cout << "Choice --> ";
     cin >> entry;
     
-    // traverse that many times and delete that node
-    current = head;
+    current = head;    // current & prev set to point at head (start of the list)
     prev = head;
     
     for (int i = 0; i < (entry-1); i++)
@@ -104,7 +105,8 @@ void deletes_node(Node * &head) {
     output(head);
 }
 
-void inserts_node(Node * &head) {
+void inserts_node(Node * &head)
+{
     Node * current = nullptr;
     Node * prev = nullptr;
     int entry;
@@ -142,7 +144,8 @@ void inserts_node(Node * &head) {
     output(head);
 }
 
-void deletes_list(Node * &head) {
+void deletes_list(Node * &head)
+{
     Node * current = head;
     while (current) {
         head = current->next;
